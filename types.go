16caed7b333f7e12b445a6d06c6ea84f1400462f
Jessie France <FranceJessieJ@gmail.com>
2022-03-24 11:42:26 -0600

    feat: add structs for repository and initial api endpoints

---
+++ types.go
@@ -0,0 +1,35 @@
+package checkgitci
+
+import "time"
+
+// Repository type holds information for individual Git repositories.
+type Repository struct {
+	Owner      string
+	Name       string
+	Sha        string
+	RunsResult CheckRunsAPI
+	Success    bool
+	Completed  bool
+	CommitsURL string
+	RunsURL    string
+}
+
+// CommitsAPI holds selected information on the response from GitHub commits API.
+type CommitsAPI []struct {
+	Sha string
+}
+
+// CheckRunsAPI holds selected information from the GitHub check-runs API.
+type CheckRunsAPI struct {
+	TotalCount int   `json:"total_count"`
+	CheckRuns  []Run `json:"check_runs"`
+}
+
+// Run holds selected information on an individual GitHub CI run.
+type Run struct {
+	Name        string    `json:"name"`
+	Status      string    `json:"status"`
+	Conclusion  string    `json:"conclusion"`
+	StartedAt   time.Time `json:"started_at"`
+	CompletedAt time.Time `json:"completed_at"`
+}
